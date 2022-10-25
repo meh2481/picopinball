fdec501e9a0afd76424d60bfeb1f894789078977
inderpxar <inderpxar@gmail.com>
2022-10-25 15:45:46 -0400

    Startup sound, IR sensors for sound controller r now

--- code/soundController/code.py
+++ code/soundController/code.py
@@ -6,7 +6,16 @@ import storage
 import audiomp3
 import audiopwmio
 import time
-from adafruit_debouncer import Debouncer
+from audiocore import WaveFile
+
+# Init audio PWM out
+print("Initializing audio PWM out...")
+audio = audiopwmio.PWMAudioOut(board.GP0)
+
+# First things first, play startup sound
+wave_file = open("sfx/SOUND1.WAV", "rb")
+wave = WaveFile(wave_file)
+audio.play(wave)
 
 playing = False
 playing_stop_timer = 0
@@ -132,11 +141,30 @@ uart = init_uart()
 print("Initializing UART for other pico...")
 uart_comm = init_uart_comm()
 
-# Init audio PWM out and music file
-print("Initializing audio PWM out...")
-audio = audiopwmio.PWMAudioOut(board.GP0)
+# Init MP3 decoder for music
 decoder = audiomp3.MP3Decoder(open("/sd/PINBALL.mp3", "rb"))
 
+# Init the IR sensors
+ir1 = digitalio.DigitalInOut(board.GP28)
+ir1.direction = digitalio.Direction.INPUT
+ir1.pull = digitalio.Pull.UP
+ir2 = digitalio.DigitalInOut(board.GP3)
+ir2.direction = digitalio.Direction.INPUT
+ir2.pull = digitalio.Pull.UP
+ir3 = digitalio.DigitalInOut(board.GP4)
+ir3.direction = digitalio.Direction.INPUT
+ir3.pull = digitalio.Pull.UP
+
+ir_sensors = [ir1, ir2, ir3]
+prev_ir_values = [False, False, False]
+RE_ENTRY_SOUND = 18
+
+print("Wait for startup sound done...")
+while audio.playing:
+    pass
+print("Startup sound done")
+wave.deinit()
+# TODO: Send startup signal to display controller
 print("Start main loop...")
 while True:
     audio.play(decoder) # Loop music forever
@@ -149,3 +177,13 @@ while True:
             readline_comm(uart_comm)
         
         playing_stop_timer -= 1
+
+        # Check IR sensors
+        for i, ir in enumerate(ir_sensors):
+            if not ir.value:
+                if not prev_ir_values[i]:
+                    print(f"IR sensor {i} triggered")
+                    play_sound(uart, RE_ENTRY_SOUND)
+                    prev_ir_values[i] = True
+            else:
+                prev_ir_values[i] = False
